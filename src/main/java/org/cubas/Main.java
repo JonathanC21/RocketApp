03afc7ec6bbb7d17c0d5c2ec3b0cf259c509a830
JonathanC21 <84106612+JonathanC21@users.noreply.github.com>
2024-04-21 16:55:49 -0400

    Added docker and logger

--- src/main/java/org/cubas/Main.java
+++ src/main/java/org/cubas/Main.java
@@ -1,27 +1,37 @@
 package org.cubas;
 
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
 
+    protected static final Logger logger = LogManager.getLogger();
+
     public static void main(String[] args) {
-        double deltaVMs;
+        double deltaVMs = 0.0;
         double dryMassKg;
         double wetMassKg;
         double exhaustVelocityMs;
-        double thrustToWeightRatio;
+        double thrustToWeightRatio = 0.0;
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Type in the wet mass");
-        wetMassKg = scan.nextDouble();
-        System.out.println("Type in the dry mass");
-        dryMassKg = scan.nextDouble();
-        System.out.println("Type in the exhaust velocity");
-        exhaustVelocityMs = scan.nextDouble();
-        System.out.println("Type in the thrust to weight ratio");
-        thrustToWeightRatio = scan.nextDouble();
-        deltaVMs = calculateDeltaV(wetMassKg + dryMassKg,dryMassKg,exhaustVelocityMs);
-        System.out.println("Delta-V: " + deltaVMs + "m/s");
+        logger.info("Program started");
+
+        try {
+            System.out.println("Type in the wet mass");
+            wetMassKg = scan.nextDouble();
+            System.out.println("Type in the dry mass");
+            dryMassKg = scan.nextDouble();
+            System.out.println("Type in the exhaust velocity");
+            exhaustVelocityMs = scan.nextDouble();
+            System.out.println("Type in the thrust to weight ratio");
+            thrustToWeightRatio = scan.nextDouble();
+            deltaVMs = calculateDeltaV(wetMassKg + dryMassKg, dryMassKg, exhaustVelocityMs);
+            System.out.println("Delta-V: " + deltaVMs + "m/s");
+        }catch (Exception e) {
+            logger.error("Invalid input used");
+        }
 
         if (canGoToOrbit(deltaVMs,thrustToWeightRatio)){
             System.out.println("You will go to space today");
@@ -30,6 +40,7 @@ public class Main {
             System.out.println("You will not go to space today");
         }
 
+        logger.info("Program ended");
         System.exit(0);
     }
 
